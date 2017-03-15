aa47c0aae92082992cbe3757b7b73c30808e6edf
purushotham11 <purushothamqae@gmail.com>
2017-03-15 17:54:19 +0530

    first commit

---
+++ src/main/java/key/word/App.java
@@ -0,0 +1,13 @@
+package key.word;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/key/word/SecondAttempt.java
@@ -0,0 +1,5 @@
+package key.word;
+
+public class SecondAttempt {
+
+}
