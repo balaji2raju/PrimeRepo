f2323a379268b5103ef69caaa405b43576838c0d
balaji raju <balaji2raju@gmail.com>
2017-03-16 11:31:34 +0530

    222 moving to repository

---
+++ src/com/Repo/Test1/Test222.java
@@ -0,0 +1,12 @@
+package com.Repo.Test1;
+
+public class Test222 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am in Test 22222");
+
+	}
+
+}
